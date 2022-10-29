cf3576f5868564d96e7db1bc8f1c490376537e33
afraklaabdelaziz <afraklaabdelaziz@gmail.com>
2022-10-29 16:20:01 +0100

    modification

--- src/main/java/com/example/pdamanager/Entities/User.java
+++ src/main/java/com/example/pdamanager/Entities/User.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Table(name = "users")
 public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.TABLE)
