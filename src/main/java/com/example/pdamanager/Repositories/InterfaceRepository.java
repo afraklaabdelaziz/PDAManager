d107417c414215f89d2f5559b17c534ebfeb7fb4
afraklaabdelaziz <afraklaabdelaziz@gmail.com>
2022-11-01 09:29:58 +0100

    checkout to main

--- src/main/java/com/example/pdamanager/Repositories/InterfaceRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.pdamanager.Repositories;
-
-
-public interface InterfaceRepository<T> {
-    public T findByEmail(String email);
-}
--- src/main/java/com/example/pdamanager/Services/InterfaceService.java
+++
@@ -1,5 +0,0 @@
-package com.example.pdamanager.Services;
-
-public interface InterfaceService<T> {
-    public T findByEmail(String email);
-}
